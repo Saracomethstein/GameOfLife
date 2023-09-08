93085174622262a170d83524c96f82f7ca137bba
Saracomethstein <adel.sevastianov@yandex.ru>
2023-09-08 20:21:12 +0300

    Create rep

---
+++ ForWindows/game_of_life.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+#include <conio.h>
+
+#define ROWS 25
+#define COLS 80
+
+void initialize(int **grid, FILE *fptr);
+void display(int **grid);
+void update(int **grid);
+void memoryCleaner(int **grid);
+void gotoxy(int x, int y);
+
+int main(int argc, char *argv[]) {
+    if ((argc >= 2 && fopen(argv[1], "rt"))) {
+        int n;
+        char c;
+        int **grid = (int **)malloc(ROWS * sizeof(int *));
+
+        FILE *fptr = fopen(argv[1], "rt");
+
+        for (int i = 0; i < ROWS; i++) {
+            grid[i] = (int *)malloc(COLS * sizeof(int));
+        }
+
+        initialize(grid, fptr);
+
+        printf("Please, set the speed of steps: ");
+        if (scanf("%d%c", &n, &c) == 2 && c == '\n' && (n > 0 && n <= 1000)) {
+            while (1) {
+                display(grid);
+                update(grid);
+                Sleep(n);
+            }
+        } else {
+            printf("Please, set the correct positive number.");
+        }
+
+        memoryCleaner(grid);
+
+    } else {
+        printf("Sorry, we didn't find such file...");
+    }
+
+    return 0;
+}
+
+void initialize(int **grid, FILE *fptr) {
+    char symbol;
+
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            symbol = fgetc(fptr);
+            if (symbol != '\0' && symbol != '\n' && symbol != ' ') {
+                grid[i][j] = symbol - 48;
+            }
+        }
+    }
+}
+
+void display(int **grid) {
+    gotoxy(0, 0);
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            printf("%c", grid[i][j] ? 'O' : ' ');
+        }
+        printf("\n");
+    }
+}
+
+void update(int **grid) {
+    int newGrid[ROWS][COLS];
+
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            int neighbors = 0;
+
+            for (int dx = -1; dx <= 1; dx++) {
+                for (int dy = -1; dy <= 1; dy++) {
+                    if (dx == 0 && dy == 0) continue;
+
+                    int ni = (i + dx + ROWS) % ROWS;
+                    int nj = (j + dy + COLS) % COLS;
+
+                    if (ni >= 0 && ni < ROWS && nj >= 0 && nj < COLS) {
+                        neighbors += grid[ni][nj];
+                    }
+                }
+            }
+
+            if (grid[i][j]) {
+                newGrid[i][j] = (neighbors == 2 || neighbors == 3);
+            } else {
+                newGrid[i][j] = (neighbors == 3);
+            }
+        }
+    }
+
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            grid[i][j] = newGrid[i][j];
+        }
+    }
+}
+
+void memoryCleaner(int **grid) {
+    for (int i = 0; i < ROWS; i++) {
+        free(grid[i]);
+    }
+    free(grid);
+}
+
+void gotoxy(int x, int y) {
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
